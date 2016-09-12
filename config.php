9c690db96eddd5642ef9d18e8fbf456b8c5d4133
qiaoer <qiaoer2017@163.com>
2016-09-12 15:32:35 +0800

    配置文件和数据库连接文件完成

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiaoer
+ * Date: 16/9/12
+ * Time: 15:22
+ */
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiaoer
+ * Date: 16/9/12
+ * Time: 15:23
+ */
+include 'config.php';
+$conn = mysql_connect(HOST, USERNAME, PASSWORD);
+if (!$conn) {
+    echo mysql_error();
+}
+if (!mysql_select_db('my_test', $conn)) {
+    echo mysql_error();
+}
+if (!mysql_query("set names utf8")) {
+    echo mysql_error();
+}
